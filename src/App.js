64b55c544a6db4ccc418f278eb07c975d1f8accd
VietIS Tan <tan.hoangngoc@vietis.com.vn>
2021-03-02 13:34:57 +0700

    react_hook_bai_6_get_api_using_use_effect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
 import TodoList from "./components/TodoList";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TodoForm from "./components/TodoForm";
+import PostList from "./components/PostList";
 
 function App() {
   const [todoList, setTodoList] = useState([
@@ -10,6 +11,32 @@ function App() {
     { id: 2, title: "data2" },
     { id: 3, title: "data3" },
   ]);
+
+  const [postList, setpostList] = useState([]);
+// tất cả effect đều chạy sau render
+  useEffect(() => { // chạy trước 1
+    async function fetchPostList() {
+      try {
+        const requestUrl =
+          "http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1";
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+        console.log(responseJSON);
+
+        const { data } = responseJSON;
+        setpostList(data);
+      } catch (error) {
+        console.log("failded to fetch post list");
+      }
+    }
+    console.log('POST list effect') // sẽ không chạy lại lần thứ 2
+    fetchPostList();
+  }, []); //chạy đúng 1 lần duy nhất
+
+  useEffect(() => { // chạy sau 2
+    console.log('TODO list effect') // luôn luôn chạy sau mỗi lần render
+  })
+
   function handleTodoClick(todo) {
     console.log(todo);
     const index = todoList.findIndex((x) => x.id === todo.id);
@@ -35,8 +62,10 @@ function App() {
 
   return (
     <div className="App">
+      <h2>post list</h2>
       <TodoForm onSubmit={handleTodoFormSubmit} />
       <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      <PostList posts={postList} />
     </div>
   );
 }
