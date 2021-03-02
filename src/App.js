87c7159a6cf278e0c355010350341de3e1bb6887
VietIS Tan <tan.hoangngoc@vietis.com.vn>
2021-03-02 16:28:15 +0700

    react hook bai 8 pagination using useEffect

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import TodoList from "./components/TodoList";
 import { useState, useEffect } from "react";
 import TodoForm from "./components/TodoForm";
 import PostList from "./components/PostList";
+import Pagination from "./components/Pagination";
+import queryString from "query-string";
 
 function App() {
   const [todoList, setTodoList] = useState([
@@ -13,29 +15,49 @@ function App() {
   ]);
 
   const [postList, setpostList] = useState([]);
-// tất cả effect đều chạy sau render
-  useEffect(() => { // chạy trước 1
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 1,
+  });
+  const [filters, setfilters] = useState({
+    _limit: 10,
+    _page: 1,
+  });
+
+  // tất cả effect đều chạy sau render
+  useEffect(() => {
+    // chạy trước 1
     async function fetchPostList() {
       try {
-        const requestUrl =
-          "http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1";
+        const paramsString = queryString.stringify(filters);
+        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
         const response = await fetch(requestUrl);
         const responseJSON = await response.json();
         console.log(responseJSON);
 
-        const { data } = responseJSON;
+        const { data, pagination } = responseJSON;
         setpostList(data);
+        setPagination(pagination);
       } catch (error) {
         console.log("failded to fetch post list");
       }
     }
-    console.log('POST list effect') // sẽ không chạy lại lần thứ 2
+    console.log("POST list effect"); // sẽ không chạy lại lần thứ 2
     fetchPostList();
-  }, []); //chạy đúng 1 lần duy nhất
+  }, [filters]); //chạy đúng 1 lần duy nhất
 
-  useEffect(() => { // chạy sau 2
-    console.log('TODO list effect') // luôn luôn chạy sau mỗi lần render
-  })
+  useEffect(() => {
+    // chạy sau 2
+    console.log("TODO list effect"); // luôn luôn chạy sau mỗi lần render
+  });
+
+  function handlePageChange(newPage) {
+    setfilters({
+      ...filters,
+      _page: newPage,
+    });
+  }
 
   function handleTodoClick(todo) {
     console.log(todo);
@@ -66,6 +88,7 @@ function App() {
       <TodoForm onSubmit={handleTodoFormSubmit} />
       <TodoList todos={todoList} onTodoClick={handleTodoClick} />
       <PostList posts={postList} />
+      <Pagination pagination={pagination} onPageChange={handlePageChange} />
     </div>
   );
 }
